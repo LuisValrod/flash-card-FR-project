a0d14409962add5327f4827c6316e460b2f29b1f
LuisValrod <luisrodriguezvalido@gmail.com>
2024-03-05 07:49:39 +0000

    timer, and flipping cards

--- main.py
+++ main.py
@@ -9,26 +9,43 @@ import random
 #read data
 words_data = pd.read_csv('data/french_words.csv')
 words_dict = words_data.to_dict(orient='records')
+pd.DataFrame.to_csv(words_data, 'word_to_learn.csv')
 print(words_dict)
 
+current_card=''
 # function to change words
 def changingCards():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    canvas.itemconfig(card_can, image=card_front)
     current_card = random.choice(words_dict)
     fr_word = current_card['French']
-    canvas.itemconfig(title, text='French')
-    canvas.itemconfig(word, text=fr_word)
+    canvas.itemconfig(title, text='French', fill='black')
+    canvas.itemconfig(word, text=fr_word, fill='black')
     print(fr_word)
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    global current_card
+    en_word = current_card['English']
+    canvas.itemconfig(card_can, image=card_back)
+    canvas.itemconfig(title, fill='white', text='English')
+    canvas.itemconfig(word, fill='white', text=en_word)
+
+
 
 #------------ SET UP UI ------------------------#
 window = Tk()
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 window.title('My FlashCard Project')
+flip_timer = window.after(3000, func=flip_card)
 
 #Canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file='images/card_front.png')
 card_back = PhotoImage(file='images/card_back.png')
-canvas.create_image(410, 261, image=card_front)
+card_can = canvas.create_image(410, 261, image=card_front)
 title = canvas.create_text(400, 150, text='Title', font=('Arial', 40, 'italic'))
 word = canvas.create_text(400, 263, text=TEXT, font=('Arial', 60, 'bold'))
 
@@ -46,4 +63,5 @@ btn_wrong.grid(column=0, row=1)
 
 
 changingCards()
+
 window.mainloop()
